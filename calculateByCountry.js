10e4ee4fb9a4ac132233a759a98919a7dcc281e1
elisabiver <44230413+elisabiver@users.noreply.github.com>
2019-04-04 19:49:47 +0200

    exo node

--- calculateByCountry.js
+++ calculateByCountry.js
@@ -11,3 +11,18 @@ const writeJson = require('./writeJson')
 */
 const DATA = require('./asylum.json')
 // asylum.json doit être créé avec prepareData
+
+
+let countries = R.uniq(DATA.map(d=>d.country_asylum));
+
+const calcSomme = (resultat, d) => {return resultat + d} //return si tu mets les {} 
+
+const SOMME = pays => { return DATA.filter(d=>d.country_asylum == pays)
+                          .map(d=>d.affected)
+                          .reduce(calcSomme,0)}
+
+//console.log(countries.map(pays => ({pays, somme:SOMME(pays)}))); 
+
+const list = countries.map(pays => ({pays, somme:SOMME(pays)}))
+
+writeJson('asylumByCountry.json', list)
