b37c16d507064aa47841ff8b20d33fdcfba0868d
elisabiver <44230413+elisabiver@users.noreply.github.com>
2019-04-04 18:55:29 +0200

    Update prepareData.js

--- prepareData.js
+++ prepareData.js
@@ -3,14 +3,12 @@ const d3 = require('d3')
 const R = require('ramda')
 
 const writeJson = require('./writeJson')
-/*
-  writeJson prends deux arguments:
-    * le nom du fichier
-    * un objet ou une liste JSON
-  
-  ex:
-    writeJson('asylum.json', data)
-*/
 
 const URL = 'https://raw.githubusercontent.com/idris-maps/heig-datavis-2019/master/20190322-node/exercice_node/ch_asylum_demands.csv'
-// le lien vers le fichier CSV
+
+fetch(URL).then(r=>r.text()) 
+.then(d3.csvParse)
+.then(r=>r.filter(t => t.affected !=="*"))
+.then(data => data.map(d =>({...d,year:Number(parseInt(d.year, 10)),affected:Number(parseInt(d.affected, 10)) }))) 
+.then(data => data.map(d =>({...d,country_asylum: d.country_asylum.includes("USA") ? "USA" : d.country_asylum}))) 
+.then(data => writeJson('asylum.json', data))
